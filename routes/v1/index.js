234b9a876a7dc65679ee87d23dc24a671fc04a49
Arnob Almazee <arnobxtreme@gmail.com>
2020-12-22 09:33:04 +0600

    updated

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -8,7 +8,7 @@ router.use('/ppp', require('./ppp/ppp_secret_disable'));
 router.use('/ppp', require('./ppp/ppp_secret_enable'));
 router.use('/ppp', require('./ppp/ppp_interface_remove'));
 router.use('/ppp', require('./ppp/interface_ethernet_speed_print'));
-
+router.use('/ppp', require('./ppp/ppp_profile_print'));
 
 
 module.exports = router;
