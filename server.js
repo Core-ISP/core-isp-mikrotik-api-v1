1ded76ca00804029800fafbd3fa974d80d02a436
Arnob Almazee <arnobxtreme@gmail.com>
2021-02-25 12:55:47 +0600

    updating

--- server.js
+++ server.js
@@ -8,12 +8,22 @@ app.use(cors())
 app.use(BodyParser.urlencoded({ extended: false }))
 app.use(BodyParser.json())
 
-// Routers
+// NOTE Routers
 app.use(PATH, require('./routes'))
 app.get(PATH, (req, res) => res.send('CORE ISP Mikrotik Ok'))
 
-// NOTE Request
+
+// NOTE GET Request Not Found
 app.get('*', function (req, res) {
+    console.log("Not Found Page")
+    res.json({
+        success: false,
+        message: 'No Route Found'
+    })
+});
+
+// NOTE POST Request Not Found
+app.post('*', function (req, res) {
     console.log("Not Found Page")
     res.json({
         success: false,
